b01f28f623caf27841129ab734af2e0b9d4447c3
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-04-07 20:32:46 -0400

    done

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
+import { envs } from "./config/envs";
 import { Server } from "./presentation";
 
 ( async() => {
     main();
-}) ();
+} ) ();
 
 function main() {
 
-    const server = new Server();
+    const server = new Server({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH
+    });
+
     server.start();
 }
 
