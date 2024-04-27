bf33fc2788732983d4058134925d8616ce29efe4
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-04-27 11:54:40 -0400

    Implement clean architecture and add domain and infrastructure folders

--- README.md
+++ README.md
@@ -20,8 +20,13 @@ $ npx prisma migrate dev --name init
 
 
 ## Implementar Arquitectura limpia
+# Dominio
+Siguiendo el principio de Domain Driven Design
 1. En domain, crear nuestra entidad
 2. Datasources, sera nuestro origen de datos
 3. Repositories, sera nuestros metodos para llegar a datasources
 4. Son clases abstractas o interfaces. En el fondo, lo que buscamos es obligar que nuestros objetos luzcan de la apariencia de nosotros esperamos.
+
+# Infrastructure
+Las implementaciones se colocan dentro de infrastructure
 
---
+++ src/infrastructure/datasource/todo.datasource.impl.ts
@@ -0,0 +1,44 @@
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, TodoDataSource, TodoEntity, UpdateTodoDto } from "../../domain";
+
+export class TodoDataSourceImpl implements TodoDataSource {
+    async create(createTodoDto: CreateTodoDto): Promise<TodoEntity> {
+        const createTodo = await prisma.todo.create({
+            data: createTodoDto!
+        })
+        return TodoEntity.fromObject(createTodo);
+    }
+    
+    async getAll(): Promise<TodoEntity[]> {
+        const todos = await prisma.todo.findMany();
+        return todos.map( todo => TodoEntity.fromObject(todo) );
+    }
+
+    async findById(id: number): Promise<TodoEntity> {
+        const todo = await prisma.todo.findFirst({
+            where: { id }
+        });
+
+        if(!todo) throw `Todo with id ${ id } not found`
+        throw TodoEntity.fromObject(todo);
+    }
+
+    async updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity> {
+        await this.findById( updateTodoDto.id );
+        const updateTodo = await prisma.todo.update({
+            where: { id: updateTodoDto.id },
+            data: updateTodoDto!.values
+        });
+
+        return TodoEntity.fromObject(updateTodo);
+    }
+
+    async deleteById(id: number): Promise<TodoEntity> {
+        await this.findById( id );
+        const deleted = await prisma.todo.delete({
+            where: { id: id }
+        });
+        return TodoEntity.fromObject(deleted);
+    }
+
+}
