44e789a36bd56611affa33aa1b10ebfb3fc81e6f
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-04-26 21:20:52 -0400

    Update todos controller and routes

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -2,10 +2,10 @@
 import { Request, Response } from 'express';
 
 const todos = [
-    { id: 1, text: 'Buy milk',   createdAt: new Date() },
-    { id: 2, text: 'Buy bread',  createdAt: new Date() },
-    { id: 3, text: 'Buy cheese', createdAt: new Date() },
-    { id: 4, text: 'Buy butter', createdAt: new Date() },
+    { id: 1, text: 'Buy milk',   completedAt: new Date() },
+    { id: 2, text: 'Buy bread',  completedAt: new Date() },
+    { id: 3, text: 'Buy cheese', completedAt: null },
+    { id: 4, text: 'Buy butter', completedAt: new Date() },
 ]
 
 export class TodosController {
@@ -27,10 +27,45 @@ export class TodosController {
         ( todo )
             ? res.json(todo)
             : res.status(404).json({ error: `Todo with id ${id} not found` })
-    }
+    };
 
     public createTodo = ( req: Request, res: Response ) => {
-        const body = req.body;
-        res.json({ message: body })
+        const { text } = req.body;
+        if ( !text ) return res.status(400).json({ error: 'Text is required' });
+
+        const newTodo = {
+            id: todos.length + 1,
+            text: text,
+            completedAt: null
+        }
+
+        todos.push( newTodo );
+        res.json({ message: newTodo })
+    };
+
+    public updateTodo = (req: Request, res: Response ) => {
+        const id = +req.params.id;
+        if ( isNaN(id) ) return res.status(400).json({ error: 'ID Argument must be a number'})
+
+        const todo = todos.find(todo => todo.id === id);
+        if (!todo) return res.status( 404 ).json( { error: `Todo with id ${id} not found` } );
+        
+        const { text, completedAt } = req.body;
+        
+        todo.text = text || todo.text; // esto no se hace, la mutacion
+
+        ( completedAt === 'null')
+            ? todo.completedAt = null
+            : todo.completedAt = new Date( completedAt || todo.completedAt );  // OJO, REFERENCIA
+            
+        // todos.forEach( (todo, index) => {
+        //     if ( todo.id === id ) todos[index] = todo;
+        // })
+
+        res.json({ message: todo })
+    }
+
+    public deleteTodo = (req: Request, res: Response ) => {
+        res.json({ message: 'deleteTodo' })
     }
 }
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -4,19 +4,17 @@ import { TodosController } from "./controller";
 //* Estas son las rutas de mis TODOS
 //* SIEMPRE Pensar que nadie sabe como usar el backend y lo usaran mal
 export class TodoRoutes {
-
     static get routes(): Router {
         const router = Router();
         const todoController = new TodosController();
 
         // Solo pasamos la referencia a la funcion ya que tenemos los mismos argumentos que la funcion
-        
-        router.get( '/'   , todoController.getTodos );
-        router.get( '/:id', todoController.getTodoById );
-        router.post( '/'  , todoController.createTodo );
-
+        // Nuestros endpoints del todo
+        router.get( '/'     , todoController.getTodos    );
+        router.get( '/:id'  , todoController.getTodoById );
+        router.post( '/'    , todoController.createTodo  );
+        router.put( '/:id'  , todoController.updateTodo  );
+        router.delete('/:id', todoController.deleteTodo  );
         return router;
     }
-
-
 }
